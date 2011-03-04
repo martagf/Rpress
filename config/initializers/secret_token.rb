e8a7dd78726fac0c0ff88bd1e1952765b3909b1a
Marta <marta.gferrero@gmail.com>
2011-03-04 21:44:52 +0000

    skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpress::Application.config.secret_token = '6b30ed3ea0c2380d9f2550f9998aaf3bad9e8e8903eb2876e0e44bc9f74e65ac04a6fed091ba6a958334b183280b6a6ef03ddebaad2df39dab3652bbf20e3dcf'
